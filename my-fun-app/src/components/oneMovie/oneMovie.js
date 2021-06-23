e6552743cfe9bcfb3a6b3770fb58ed6563560c5b
Icarus <ibuckhold@gmail.com>
2021-06-23 12:15:10 -0600

    connecting to API with API key

--- my-fun-app/src/components/oneMovie/oneMovie.js
+++ my-fun-app/src/components/oneMovie/oneMovie.js
@@ -2,6 +2,12 @@ import react, { useState } from 'react';
 
 function OneMovie() {
 
+  async function getOneMovie() {
+    const data = await fetch(`https://api.themoviedb.org/https://api.themoviedb.org/3/movie/popular?api_key=7a72b6558ac960d91c238471d0b9c4a5`)
+    console.log(data)
+  }
+
+
   return (
     <>
       <div>hey</div>
---
+++ my-fun-app/src/store/oneMovie.js
@@ -0,0 +1,3 @@
+const getOneMovieInfo => async () => {
+
+}
